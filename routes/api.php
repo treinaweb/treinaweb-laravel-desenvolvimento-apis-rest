efc07706a8f504f25e35157091aa31009c6da9e7
Elton Fonseca <elton869@gmail.com>
2022-02-19 13:08:19 -0300

    07 - Criando a lista de alunos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/alunos', function () {
-    return 'lista de alunos';
-})->name('alunos.index');
+Route::get('/alunos', [AlunoController::class, 'index'])->name('alunos.index');
 
 Route::get('/alunos/{id}', function () {
     return 'detalhes do aluno';
