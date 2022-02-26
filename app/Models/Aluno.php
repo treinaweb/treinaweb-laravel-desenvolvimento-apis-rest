47b5a8bf6d61ce6808caf41f2189f64d5ca23446
Elton Fonseca <elton869@gmail.com>
2022-02-26 11:40:53 -0300

    24 - Enviando atributos virtuais no momento da serialização

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -10,15 +10,41 @@ class Aluno extends Model
 {
     use HasFactory;
 
+    /**
+     * Indica os campos que podem ter valor definidos via 
+     * definição de dados em massa
+     *
+     * @var array
+     */
     protected $fillable = ['nome', "nascimento", "genero", "turma_id"];
 
+    /**
+     * Defini as conversões no momento da serialização
+     *
+     * @var array
+     */
     protected $casts = [
         'nascimento' => 'date:d/m/Y'
     ];
 
+    /**
+     * Define os campos escondidos no momento da serialização
+     */
     //protected $hidden = ['created_at', 'updated_at'];
 
-    protected $visible = ['id', 'nome', 'genero', 'turma_id'];
+    /**
+     * Define os campos enviados no momento da serialização
+     *
+     * @var array
+     */
+    protected $visible = ['id', 'nome', 'nascimento', 'genero', 'turma_id', 'aceito'];
+
+    /**
+     * Define os acessores enviados na serialização
+     *
+     * @var array
+     */
+    protected $appends = ['aceito'];
 
     /**
      * Define a relação entre aluno e turma
@@ -28,5 +54,15 @@ class Aluno extends Model
     public function turma(): BelongsTo
     {
         return $this->belongsTo(Turma::class);
+    }
+
+    /**
+     * Cria um atributo virtual chamado aceito
+     *
+     * @return string
+     */
+    public function getAceitoAttribute(): string
+    {
+        return $this->attributes['nascimento'] > '2001-01-01' ? 'aceito' : 'Não Aceito';
     }
 }
