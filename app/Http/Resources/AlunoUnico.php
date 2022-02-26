1ec679f17a181ce988773bd1f6e0c661f0f0c669
Elton Fonseca <elton869@gmail.com>
2022-02-26 15:30:20 -0300

    32 - Implementando o HATEOAS

--- app/Http/Resources/AlunoUnico.php
+++ app/Http/Resources/AlunoUnico.php
@@ -16,7 +16,24 @@ class AlunoUnico extends JsonResource
     {
         return [
             'nome_aluno' => $this->nome,
-            'turma' => new TurmaResource($this->whenLoaded('turma'))
+            'turma' => new TurmaResource($this->whenLoaded('turma')),
+            'links' => [
+                [
+                    'type' => 'GET',
+                    'url' => route('alunos.show', $this->id),
+                    'rel' => 'aluno_detalhes'
+                ],
+                [
+                    'type' => 'PUT',
+                    'url' => route('alunos.update', $this->id),
+                    'rel' => 'aluno_atualizar'
+                ],
+                [
+                    'type' => 'DELETE',
+                    'url' => route('alunos.destroy', $this->id),
+                    'rel' => 'aluno_remover'
+                ]
+            ]
         ];
     }
 }
